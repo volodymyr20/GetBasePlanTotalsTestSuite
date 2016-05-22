7e2fb9367978477062516a21d6651cfd100b91c6
volodymyr20 <volodymyrbekesha@gmail.com>
2016-05-22 17:11:04 +0200

    moving hard coded values to constants

--- LoginPage.py
+++ LoginPage.py
@@ -3,11 +3,18 @@ from selenium.webdriver.common.by import By
 from Base_Page_Object2 import Page
  
 class LoginPage(Page):
+    #local constants
+    USER_EMAIL_ID='user_email'
+    USER_PASSWORD_ID='user_password'
+    LOGIN_BUTTON_ID='user_new'
+    
+    USER_NAME='volodymyrbekesha@gmail.com'
+    USER_PASSWORD='volo20dymyr'
+    
     #Locators
-    UserEmail_loc = (By.ID, 'user_email')
-    UserPassword_loc = (By.ID, 'user_password')
-    LogInBtn_loc = (By.ID, 'user_new')
-    #LogInBtn_loc = (By.TAG_NAME, 'Log in')
+    UserEmail_loc = (By.ID, USER_EMAIL_ID)
+    UserPassword_loc = (By.ID, USER_PASSWORD_ID)
+    LogInBtn_loc = (By.ID, LOGIN_BUTTON_ID)
 
     def __init__(self,driver=None):
         self.driver = driver
@@ -18,8 +25,8 @@ class LoginPage(Page):
         """Returns the page title"""        
         return self.driver.title
     def LogIn(self):
-        self.find_element(*self.UserEmail_loc).send_keys('volodymyrbekesha@gmail.com')
-        self.find_element(*self.UserPassword_loc).send_keys('volo20dymyr')  
+        self.find_element(*self.UserEmail_loc).send_keys(self.USER_NAME)
+        self.find_element(*self.UserPassword_loc).send_keys(self.USER_PASSWORD)  
 
         self.find_element(*self.LogInBtn_loc).submit()
  
